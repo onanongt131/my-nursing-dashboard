98a28dc5612289b4a6435a7a2f38ab96b8b7ebb9
onanongt131 <onanongt131@gmail.com>
2026-07-07 21:24:10 +0700

    fix: update supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,26 +1,10 @@
-// ตัวอย่างการสร้าง Client ที่ถูกต้อง
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { createClient } from '@supabase/supabase-js'
 
-export async function createClient() {
-  const cookieStore = await cookies()
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll()
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {}
-        },
-      },
-    }
-  )
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase Environment Variables")
 }
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
